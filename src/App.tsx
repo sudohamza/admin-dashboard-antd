6e09956c23d637d8e2830c6945974b58c11e4207
hamza ali <askhamzaali@gmail.com>
2024-02-18 06:40:40 +0500

    home-layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
 import React, { useState } from "react";
-import { Layout, theme } from "antd";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, Button, theme } from "antd";
 
-const { Header, Content } = Layout;
+const { Header, Sider, Content } = Layout;
 
 const App: React.FC = () => {
-  return <div>Dashboard Ant-Design</div>;
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider breakpoint="lg" trigger={null} collapsedWidth={0}></Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}></Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
 };
 
 export default App;
